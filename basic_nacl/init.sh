c85a09ebe9a35f27a37aa14f1f91be51b291c0a1
Jeremy Leung <jml230@pitt.edu>
2022-01-05 13:38:19 -0500

    combine bstates and  sstates into sbsstates

--- basic_nacl/init.sh
+++ basic_nacl/init.sh
@@ -8,9 +8,9 @@ rm -rf traj_segs seg_logs istates west.h5
 mkdir   seg_logs traj_segs istates
 
 # Set pointer to bstate and tstate
-BSTATE_ARGS="--bstate-file $WEST_SIM_ROOT/bstates/bstates.txt"
+BSTATE_ARGS="--bstate-file $WEST_SIM_ROOT/sbstates/bstates.txt"
 TSTATE_ARGS="--tstate-file $WEST_SIM_ROOT/tstate.file"
-BSTATE_ARGS="--sstate-file $WEST_SIM_ROOT/sstates/sstates.txt"
+BSTATE_ARGS="--sstate-file $WEST_SIM_ROOT/sbstates/sstates.txt"
 
 # Run w_init
 w_init \
