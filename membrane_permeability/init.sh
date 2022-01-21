1c2726e0436c91fe0fabac65a9cc00912426199a
She Zhang <she.zhang@eyesopen.com>
2022-01-21 15:14:33 -0700

    minor change

--- membrane_permeability/init.sh
+++ membrane_permeability/init.sh
@@ -11,9 +11,9 @@ mkdir   seg_logs traj_segs istates
 BSTATE_ARGS="--bstate-file $WEST_SIM_ROOT/bstates/bstates.txt"
 TSTATE_ARGS="--tstate-file $WEST_SIM_ROOT/tstate.file"
 
-cd $WEST_SIM_ROOT/common_files
+cd common_files
 python create_system.py
-cd -
+cd ..
 
 # Run w_init
 w_init \
