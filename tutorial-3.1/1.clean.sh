0491e00b3d24a05046bb507f8e79caf6a89bc69c
Jeremy Leung <jml230@pitt.edu>
2022-05-20 14:29:02 -0400

    fix up jupyter notebook

--- tutorial-3.1/1.clean.sh
+++ tutorial-3.1/1.clean.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 rm -rf seg_logs traj_segs *out *log ./west.h5 pdist.h5 *.pdf for_analysis/0* ANALYSIS/ __pycache__/
 rm -rf w_crawl/traj_segs/parent_*/ w_crawl/example.h5 w_crawl/w_crawl.log
