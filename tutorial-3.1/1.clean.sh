5608c15f8da287fe5b83d11d51e583a5dddc8da9
Jeremy Leung <jml230@pitt.edu>
2022-05-20 13:10:12 -0400

    minor fix to run/init/clean files

--- tutorial-3.1/1.clean.sh
+++ tutorial-3.1/1.clean.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-rm -rf seg_logs traj_segs *out *log ./west.h5 pdist.h5 *.pdf for_analysis/0* ANALYSIS/ 
+rm -rf seg_logs traj_segs *out *log ./west.h5 pdist.h5 *.pdf for_analysis/0* ANALYSIS/ __pycache__/
 rm -rf w_crawl/traj_segs/parent_*/ w_crawl/example.h5 w_crawl/w_crawl.log
 rm -rf multi.h5
