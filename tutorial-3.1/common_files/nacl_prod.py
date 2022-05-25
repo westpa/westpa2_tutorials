d407a91e7a6b88d722d64e98eee630051488d115
Anthony Bogetti <anthony.bogetti@gmail.com>
2022-05-25 10:16:45 -0400

    minor file fixes (tstate, openmm imports, bin defs)

--- tutorial-3.1/common_files/nacl_prod.py
+++ tutorial-3.1/common_files/nacl_prod.py
@@ -1,6 +1,6 @@
-from openmm.app import *
-from openmm import *
-from openmm.unit import *
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.openmm.unit import *
 from sys import stdout
 
 pdb = PDBFile('bstate.pdb')
